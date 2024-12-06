e7f09e588dcec3be8af0b97aa062987538fd2c95
FKyleCarlos <francis.kylecarlos@gmail.com>
2024-12-05 17:46:57 -0700

    JS and CSS edits - Applied semi-colons properly in JS files - Edited font-weight: 200px into 200 - Edited align-items: left into flex-start

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,15 +1,15 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
-var submitButton = document.getElementById("submit-button")
-var mainID = document.getElementById("contact-page")
+var submitButton = document.getElementById("submit-button");
+var mainID = document.getElementById("contact-page");
 
-var thankyouText = "<p>Thank you for your message!</p>"
-submitButton.addEventListener("click", function() {changeText()});
+var thankyouText = "<p>Thank you for your message!</p>";
+submitButton.addEventListener("click", function() {changeText();});
 
 function changeText (){
     mainID.innerHTML = thankyouText;
 
-    var mainPar = document.querySelector(".contact p")
+    var mainPar = document.querySelector(".contact p");
     mainPar.style["font-size"] = "24px";
 }
